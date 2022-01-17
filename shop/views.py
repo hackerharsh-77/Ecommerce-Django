58a7b278712e5265049c94f32e719e1f06161a8e
hackerharsh-77 <r.harsh0177@gmail.com>
2022-01-17 10:28:58 +0530

    dynamic categories added

--- shop/views.py
+++ shop/views.py
@@ -5,11 +5,22 @@ from math import ceil
 # Create your views here.
 
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    params = {'no_of_Slides': nSlides, 'range': range(1,nSlides), 'product':products}
+   # products = Product.objects.all()
+    #print(products)
+    #n = len(products)
+    #nSlides = n//4 + ceil((n/4)-(n//4))
+    #allProds=[[products, range(1, nSlides), nSlides],[products, range(1, nSlides), nSlides]]
+    products= Product.objects.all()
+    allProds=[]
+    catprods= Product.objects.values('category', 'product_id')
+    cats= {item["category"] for item in catprods}
+    for cat in cats:
+        prod=Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds}
+    #params = {'no_of_Slides': nSlides, 'range': range(1,nSlides), 'product':products}
     return render(request,'shop/index.html',params)
 
 def about(request):
